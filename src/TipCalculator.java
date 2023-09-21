35ca7ebc8fa33bf2389b429199aae9582f43ac5f
BT_1N3_06 <sample@bths.edu>
2023-09-21 10:36:56 -0400

    fixed errors

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-    import java.util.Scanner
+    import java.util.Scanner;
     public class TipCalculator {
         public static void main(String [] args) {
             Scanner scan = new Scanner (System.in) ;
