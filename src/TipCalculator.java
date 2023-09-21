b8e73a57f5181ba8db20870502096c2fcdd48870
BT_1N3_06 <sample@bths.edu>
2023-09-21 10:35:17 -0400

    added statements.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,14 @@
-public class TipCalculator {
+import java.util.Scanner;
+    import java.util.Scanner
+    public class TipCalculator {
+        public static void main(String [] args) {
+            Scanner scan = new Scanner (System.in) ;
+            System.out.print("Enter your name: ");
+            String name = scan.nextLine();
+            System.out.println("Welcome " + name);
+            System.out.print("How many people are in your party? " );
+
+
+        }
+    }
 }
